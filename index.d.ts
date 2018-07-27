226ea77a8cf7230c3a46a4ed7527657f66d87640
James Kusachi <jkusachi@gmail.com>
2018-07-27 13:29:10 -0700

    typescript typings v1

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+export as namespace ReduxActionsAPIAddon;
+
+type Method = "GET" | "POST" | "PUT" | "DELETE";
+
+type returnType = string | number | object;
+
+type ForwardFunctionToString = (...args: returnType[]) => string;
+type ForwardFunctionToDynamic = (...args: returnType[]) => returnType;
+
+type ActionFunction<P1> = (...P1) => returnType;
+
+export function createAPIAction<
+  ActionArg,
+  MethodArg,
+  EndpointArg,
+  PayloadArg,
+  MetaArg
+>(
+  actionType: string,
+  method: Method,
+  endpoint: string | ForwardFunctionToString,
+  payload?: ForwardFunctionToDynamic,
+  meta?: ForwardFunctionToDynamic
+): ActionFunction<any>;
